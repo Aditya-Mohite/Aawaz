559ec2936a0fd6cff031a82887b2aef74454ab39
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-08-16 02:01:51 +0530

    Delete Chhavi directory

--- Chhavi/language page/loader.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Wait for the DOM to be fully loaded
-    setTimeout(function () {
-        // Hide the loader after 3 seconds
-        document.getElementById("loaderContainer").style.display = "none";
-    }, 3000);
-});
